3d7ea5e92b69c4adbdd39e54d6366052d77f584d
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2016-04-16 14:27:36 +0200

    D - done

--- D/main.cpp
+++ D/main.cpp
@@ -98,9 +98,9 @@ typedef Delaunay::Edge_iterator Edge_iterator; //iterator po krawedziach grafu D
 typedef Delaunay::Vertex_handle Vertex_handle; //iterator po punktach grafu Delaunaya
 
 static int n;
-static vector<Point> input;
+static vector <Point> input;
 
-static vector<vector<int>> G;   // graf kandydatów
+static vector <vector<int>> G;   // graf kandydatów
 
 static double dist(int v, int w)
 {
@@ -121,13 +121,13 @@ static void buildG()
     const int nbNeighbors = 10;
     G.resize(n);
     Delaunay D;
-    vector<pair<Point, int>> delonePoints;
+    vector <pair<Point, int>> delonePoints;
     assert(input.size() == n);
     for (int i = 0; i < n; ++i) {
         delonePoints.push_back(make_pair(input[i], i));
     }
     D.insert(delonePoints.begin(), delonePoints.end());
-    vector<unordered_set<int>> Gproto(n);
+    vector <unordered_set<int>> Gproto(n);
     for (auto i = D.finite_vertices_begin(), e = D.finite_vertices_end(); i != e; ++i) {
         vector<int> neighbors;
         auto vc = i->incident_vertices();
@@ -137,7 +137,7 @@ static void buildG()
                 neighbors.push_back(v.info());
             }
         }
-        vector<Vertex_handle> near;
+        vector <Vertex_handle> near;
         D.nearest_neighbors(i, 1 + nbNeighbors, back_inserter(near));
         assert(near.size() >= min(nbNeighbors, n - 1));
         for (auto &v : near) {
@@ -164,13 +164,12 @@ static void buildG()
 
 namespace std {
     template<>
-    struct hash<pair<int, int>>
+    struct hash<pair < int, int>> {
+    size_t operator()(const pair<int, int> &p) const noexcept
     {
-        size_t operator()(const pair<int, int> &p) const noexcept
-        {
-            return size_t(p.first) << 32 | size_t(p.second);
-        }
-    };
+        return size_t(p.first) << 32 | size_t(p.second);
+    }
+};
 }
 
 namespace std {
@@ -187,13 +186,15 @@ namespace std {
 
 static mt19937 randomEngine(time(0));
 
-static long getHamiltonianLength(const vector<int> &hamiltonian)
+template<class ForwardIterator>
+static long getHamiltonianLength(ForwardIterator begin, ForwardIterator end)
 {
     long result = 0;
-    for (unsigned i = 1; i < hamiltonian.size(); ++i) {
-        result += euclid(hamiltonian[i - 1], hamiltonian[i]);
+    ForwardIterator j = begin, i = j++;
+    for (; j < end; ++i, ++j) {
+        result += euclid(*i, *j);
     }
-    result += euclid(hamiltonian.back(), hamiltonian.front());
+    result += euclid(*i, *begin);
     return result;
 }
 
@@ -217,14 +218,26 @@ public:
 
     uint_fast16_t after(uint_fast16_t vertex);
 
-    vector<uint16_t> dumpOrderly() const;
+    vector <uint16_t> dumpOrderly() const;
 
     void moveToEnd(uint_fast16_t vertex);
 
     void reverseAfter(uint_fast16_t vertex);
 
+    void cyclicRotateToEnd(uint_fast16_t vertex);
+
+    uint_fast16_t first() const
+    {
+        return first(root);
+    }
+
+    uint_fast16_t last() const
+    {
+        return last(root);
+    }
+
 private:
-    vector<Node> chunk;
+    vector <Node> chunk;
     uint_fast16_t root;
 
     void dispatchReversal(uint_fast16_t vertex);
@@ -235,10 +248,20 @@ private:
         return chunk[v].left;
     }
 
+    uint16_t left(uint_fast16_t v) const
+    {
+        return const_cast<SplayTree *>(this)->left(v);
+    }
+
     uint16_t &right(uint_fast16_t v)
     {
         assert(v != null);
         return chunk[v].right;
+    }
+
+    uint16_t right(uint_fast16_t v) const
+    {
+        return const_cast<SplayTree *>(this)->right(v);
     }
 
     uint16_t &parent(uint_fast16_t v)
@@ -247,10 +270,20 @@ private:
         return chunk[v].parent;
     }
 
+    uint16_t parent(uint_fast16_t v) const
+    {
+        return const_cast<SplayTree *>(this)->parent(v);
+    }
+
     bool &reversed(uint_fast16_t v)
     {
         assert(v != null);
         return chunk[v].reversed;
+    }
+
+    bool reversed(uint_fast16_t v) const
+    {
+        return const_cast<SplayTree *>(this)->reversed(v);
     }
 
     bool isParent(uint_fast16_t p, uint_fast16_t chld)
@@ -302,10 +335,14 @@ private:
     void zigZag(uint_fast16_t x);
 
     void splay(uint_fast16_t vertex);
+
+    uint_fast16_t first(uint_fast16_t v) const;
+
+    uint_fast16_t last(uint_fast16_t v) const;
 };
 
 // Ustawia left, right, reversed. Nie ustawia parent
-static uint16_t buildStructure(vector<SplayTree::Node> &chunk, uint_fast16_t begin, uint_fast16_t end)
+static uint16_t buildStructure(vector <SplayTree::Node> &chunk, uint_fast16_t begin, uint_fast16_t end)
 {
     assert(begin <= end);
     assert(end != SplayTree::null);
@@ -350,18 +387,7 @@ uint_fast16_t SplayTree::before(uint_fast16_t vertex)
     if (left(vertex) == null) {
         return null;
     }
-    auto i = left(vertex);
-    bool rev = false;
-    for (auto next = i; next != null;) {
-        i = next;
-        rev = (rev != reversed(i));
-        if (rev) {
-            next = left(i);
-        } else {
-            next = right(i);
-        }
-    }
-    return i;
+    return last(left(vertex));
 }
 
 // co szkodziłoby zrobić before zakładając wstępnie że reversed?
@@ -372,21 +398,11 @@ uint_fast16_t SplayTree::after(uint_fast16_t vertex)
     if (right(vertex) == null) {
         return null;
     }
-    auto i = right(vertex);
-    bool rev = false;
-    for (auto next = i; next != null;) {
-        i = next;
-        rev = (rev != reversed(i));
-        if (rev) {
-            next = right(i);
-        } else {
-            next = left(i);
-        }
-    }
-    return i;
+    return first(right(vertex));
 }
 
-static void dumpSplayChunk(const vector<SplayTree::Node> &chunk, uint_fast16_t v, bool reverse, vector<uint16_t> &order)
+static void dumpSplayChunk(const vector <SplayTree::Node> &chunk, uint_fast16_t v, bool reverse,
+                           vector <uint16_t> &order)
 {
     if (v == SplayTree::null) {
         return;
@@ -403,15 +419,18 @@ static void dumpSplayChunk(const vector<SplayTree::Node> &chunk, uint_fast16_t v
     }
 }
 
-vector<uint16_t> SplayTree::dumpOrderly() const
+vector <uint16_t> SplayTree::dumpOrderly() const
 {
-    vector<uint16_t> result;
+    vector <uint16_t> result;
     dumpSplayChunk(chunk, root, false, result);
     return result;
 }
 
 void SplayTree::moveToEnd(uint_fast16_t vertex)
 {
+#ifdef TEST_SPLAY
+    auto oldOrder = dumpOrderly();
+#endif
     auto prev = before(vertex);
     // wykonano splay na vertex - vertex jest w korzeniu
     assert(!reversed(vertex));
@@ -426,25 +445,79 @@ void SplayTree::moveToEnd(uint_fast16_t vertex)
         setRightChild(prev, vertex);
     }
     reversed(vertex) = true;
-    if (right(vertex)) {
+    if (right(vertex) != null) {
         reversed(right(vertex)) = !reversed(right(vertex));
     }
+#ifdef TEST_SPLAY
+    auto newOrder = dumpOrderly();
+    oldOrder.erase(find(oldOrder.begin(), oldOrder.end(), vertex));
+    oldOrder.push_back(vertex);
+    assert(oldOrder == newOrder);
+#endif
 }
 
 void SplayTree::reverseAfter(uint_fast16_t vertex)
 {
+#ifdef TEST_SPLAY
+    auto oldOrder = dumpOrderly();
+#endif
     splay(vertex);
     assert(!reversed(vertex));
     auto r = right(vertex);
     if (r != null) {
         reversed(r) = !reversed(r);
     }
+#ifdef TEST_SPLAY
+    auto newOrder = dumpOrderly();
+    reverse(find(oldOrder.begin(), oldOrder.end(), vertex) + 1, oldOrder.end());
+    assert(oldOrder == newOrder);
+#endif
+}
+
+void SplayTree::cyclicRotateToEnd(uint_fast16_t vertex)
+{
+#ifdef TEST_SPLAY
+    long oldLength;
+    {
+        auto oldOrder = dumpOrderly();
+        oldLength = getHamiltonianLength(oldOrder.begin(), oldOrder.end());
+    };
+#endif
+    splay(vertex);
+    assert(root == vertex);
+    assert(!reversed(root));
+    if (right(root) == null) {
+        return; // vertex już na końcu
+    }
+    auto f = first();
+    auto r = right(root);
+    bool needReverse = false;
+    for (auto i = f; i != root; i = parent(i))
+        needReverse = (needReverse != reversed(i));
+    right(root) = null;
+    if (needReverse) {
+        assert(right(f) == null);
+        setRightChild(f, r);
+    } else {
+        assert(left(f) == null);
+        setLeftChild(f, r);
+    }
+    reversed(r) = (reversed(r) != needReverse);
+#ifdef TEST_SPLAY
+    long newLength;
+    {
+        auto newOrder = dumpOrderly();
+        newLength = getHamiltonianLength(newOrder.begin(), newOrder.end());
+    };
+    assert(oldLength == newLength);
+#endif
 }
 
 void SplayTree::dispatchReversal(uint_fast16_t vertex)
 {
     if (reversed(vertex)) {
         reversed(vertex) = false;
+        assert(reversed(vertex) == false);
         swap(left(vertex), right(vertex));
         if (left(vertex) != null) {
             reversed(left(vertex)) = !reversed(left(vertex));
@@ -581,16 +654,169 @@ void SplayTree::splay(uint_fast16_t vertex)
             }
         }
     }
+    assert(root == vertex);
+    dispatchReversal(root); // błędnie założyłem że splay zawsze pozostawia korzeń oczyszczony z reversed
+    assert(!reversed(root));
 #ifdef TEST_SPLAY
     auto newOrder = dumpOrderly();
     assert(oldOrder == newOrder);
 #endif
 }
 
+uint_fast16_t SplayTree::first(uint_fast16_t vertex) const
+{
+    auto i = vertex;
+    bool rev = false;
+    for (auto next = i; next != null;) {
+        i = next;
+        rev = (rev != reversed(i));
+        if (rev) {
+            next = right(i);
+        } else {
+            next = left(i);
+        }
+    }
+    return i;
+}
+
+uint_fast16_t SplayTree::last(uint_fast16_t vertex) const
+{
+    auto i = vertex;
+    bool rev = false;
+    for (auto next = i; next != null;) {
+        i = next;
+        rev = (rev != reversed(i));
+        if (rev) {
+            next = left(i);
+        } else {
+            next = right(i);
+        }
+    }
+    return i;
+}
+
+class UsedEdgesSet
+{
+    struct Pair
+    {
+        uint16_t a, b;
+
+        bool operator==(Pair rhs) const noexcept
+        {
+            const Pair &lhs = *this;
+            return (lhs.a == rhs.a && lhs.b == rhs.b) || (lhs.a == rhs.b && lhs.b == rhs.a);
+        }
+    };
+
+    struct HashPair
+    {
+        size_t operator()(Pair o) const noexcept
+        {
+            return static_cast<size_t>(o.a) * o.b;
+        }
+    };
+
+    unordered_set <Pair, HashPair> usedEdges;
+
+public:
+    void addEdge(uint16_t a, uint16_t b);
+
+    bool hasEdge(uint16_t a, uint16_t b) const;
+};
+
+void UsedEdgesSet::addEdge(uint16_t a, uint16_t b)
+{
+    usedEdges.insert({a, b});
+}
+
+bool UsedEdgesSet::hasEdge(uint16_t a, uint16_t b) const
+{
+    return usedEdges.count({a, b}) == 1;
+}
+
+struct ChoiceBase
+{
+    int improvement;
+    int realImprovement;
+    uint16_t vertex;
+    uint16_t secondVertex;
+
+    ChoiceBase() : improvement(numeric_limits<int>::min())
+    { }
+
+    ChoiceBase(uint16_t vertex, uint16_t secondVertex) : vertex(vertex), secondVertex(secondVertex)
+    { }
+
+    bool empty() const
+    {
+        return improvement == numeric_limits<int>::min();
+    }
+
+    virtual void execute(SplayTree &order) const = 0;
+
+    virtual void markUsedEdges(SplayTree &order, UsedEdgesSet &usedEdges) const = 0;
+};
+
+struct NodeInsertionChoice : ChoiceBase
+{
+    NodeInsertionChoice() = default;
+
+    NodeInsertionChoice(uint16_t vertex, uint16_t before, uint16_t after, uint16_t first, uint16_t last) : ChoiceBase(
+            vertex, last)
+    {
+        improvement = euclid(before, vertex) + euclid(vertex, after) - euclid(before, after) - euclid(vertex, last);
+        realImprovement = improvement + euclid(first, last) - euclid(first, vertex);
+    }
+
+    virtual void execute(SplayTree &order) const override
+    {
+        order.moveToEnd(vertex);
+    }
+
+    virtual void markUsedEdges(SplayTree &order, UsedEdgesSet &usedEdges) const override;
+};
+
+struct TwoOptChoice : ChoiceBase
+{
+    TwoOptChoice() = default;
+
+    TwoOptChoice(uint16_t vertex, uint16_t before, uint16_t after, uint16_t first, uint16_t last) : ChoiceBase(vertex,
+                                                                                                               last)
+    {
+        improvement = euclid(vertex, after) - euclid(vertex, last);
+        realImprovement = improvement + euclid(first, last) - euclid(first, after);
+    }
+
+    virtual void execute(SplayTree &order) const override
+    {
+        order.reverseAfter(vertex);
+    }
+
+    virtual void markUsedEdges(SplayTree &order, UsedEdgesSet &usedEdges) const override;
+};
+
+void NodeInsertionChoice::markUsedEdges(SplayTree &order, UsedEdgesSet &usedEdges) const
+{
+//    usedEdges.addEdge(first, last);
+//    usedEdges.addEdge(before, vertex);
+//    usedEdges.addEdge(vertex, after);
+//    usedEdges.addEdge(before, after);
+    usedEdges.addEdge(vertex, secondVertex);
+//    usedEdges.addEdge(first, vertex);
+}
+
+void TwoOptChoice::markUsedEdges(SplayTree &order, UsedEdgesSet &usedEdges) const
+{
+//    usedEdges.addEdge(first, last);
+//    usedEdges.addEdge(vertex, after);
+    usedEdges.addEdge(vertex, secondVertex);
+//    usedEdges.addEdge(first, after);
+}
+
 int main()
 {
-    close(0);
-    open("/home/local/AA/D/testy/t00.in", O_RDONLY);
+    //close(0);
+    //open("/home/local/AA/D/testy/t00.in", O_RDONLY);
     ios_base::sync_with_stdio(false);
     int z;
     cin >> z;
@@ -602,30 +828,90 @@ int main()
         }
         buildG();
         assert(G.size() == n);
-        const int16_t size = 10000;
-        SplayTree tree(size);
-        vector<int16_t> referenceOrder(size);
-        uniform_int_distribution<uint16_t> dist(0, size - 1);
-        auto ord = tree.dumpOrderly();
-        assert(ord.size() == size);
-        for (uint16_t i = 0; i < size; ++i) {
-            referenceOrder[i] = i;
-            assert(ord[i] == i);
-        }
-        for (int i = 0; i < 1000; ++i) {
-            auto position = dist(randomEngine);
-            auto it = find(referenceOrder.begin(), referenceOrder.end(), position);
-            reverse(it + 1, referenceOrder.end());
-            tree.reverseAfter(position);
-            ord = tree.dumpOrderly();
-            assert(ord.size() == size);
-            for (uint16_t i = 0; i < size; ++i) {
-                assert(ord[i] == referenceOrder[i]);
-            }
-            assert((tree.before(position) == SplayTree::null) || (tree.before(position) == *(it - 1)));
-            assert((tree.after(position) == SplayTree::null) || (tree.after(position) == *(it + 1)));
 
+        SplayTree basePath(n);
+        uint_fast16_t currentActive = n - 1;
+
+        for (uint_fast16_t staleScore = 1; staleScore <= n; ++staleScore) {
+            SplayTree currentPath = basePath;
+            vector <shared_ptr<ChoiceBase>> decisionLog;
+            int currentBest = numeric_limits<int>::min();
+            UsedEdgesSet usedEdges;
+            for (int i = 0; i < 15; ++i) {
+                NodeInsertionChoice bestNodeInsertionChoice;
+                TwoOptChoice bestTwoOptChoice;
+                auto first = currentPath.first();
+                auto last = currentPath.last();
+                for (auto a : G[last]) {
+                    auto before = currentPath.before(a);
+                    auto after = currentPath.after(a);
+                    assert(after != SplayTree::null);
+                    // node insertion
+                    if ((before != SplayTree::null) && (!usedEdges.hasEdge(a, last))) {
+                        NodeInsertionChoice choice(a, before, after, first, last);
+                        if (bestNodeInsertionChoice.empty() ||
+                            bestNodeInsertionChoice.improvement < choice.improvement) {
+                            bestNodeInsertionChoice = choice;
+                        }
+                    }
+                    // 2-opt
+                    if (!usedEdges.hasEdge(a, last)) {
+                        TwoOptChoice choice(a, before, after, first, last);
+                        if (bestTwoOptChoice.empty() || bestTwoOptChoice.improvement < choice.improvement) {
+                            bestTwoOptChoice = choice;
+                        }
+                    }
+                }
+                unique_ptr <ChoiceBase> bestDecision;
+                if (bestNodeInsertionChoice.empty() && bestTwoOptChoice.empty()) {
+                    break; // kolejne iteracje i tak nic nie zrobią
+                } else if (!bestNodeInsertionChoice.empty() && !bestTwoOptChoice.empty()) {
+                    if (bestNodeInsertionChoice.improvement >= bestTwoOptChoice.improvement) {
+                        bestDecision.reset(new NodeInsertionChoice(bestNodeInsertionChoice));
+                    } else {
+                        bestDecision.reset(new TwoOptChoice(bestTwoOptChoice));
+                    }
+                } else if (!bestNodeInsertionChoice.empty()) {
+                    bestDecision.reset(new NodeInsertionChoice(bestNodeInsertionChoice));
+                } else {
+                    assert(!bestTwoOptChoice.empty());
+                    bestDecision.reset(new TwoOptChoice(bestTwoOptChoice));
+                }
+                bestDecision->execute(currentPath);
+                bestDecision->markUsedEdges(currentPath, usedEdges);
+                auto lastImprovement = decisionLog.empty() ? 0 : decisionLog.back()->realImprovement;
+                decisionLog.emplace_back(move(bestDecision));
+                decisionLog.back()->realImprovement += lastImprovement;
+                currentBest = max(currentBest, decisionLog.back()->realImprovement);
+            }
+            if (currentBest > 0) {
+                if (currentBest != decisionLog.back()->realImprovement) {
+                    // ostateczny rezultat powinien być najczęściej tym najlepszym (chyba) - optymalizacja
+                    currentPath = basePath;
+                    for (auto choice : decisionLog) {
+                        choice->execute(currentPath);
+                        if (choice->realImprovement == currentBest) {
+                            break;
+                        }
+                    }
+                }
+                basePath = move(currentPath);
+                staleScore = 0;
+            }
+            currentActive = (currentActive + 1) % n;
+            basePath.cyclicRotateToEnd(currentActive);
         }
+
+        auto order = basePath.dumpOrderly();
+        for (auto v : order) {
+            cout << v << ' ';
+        }
+        cout << '\n' << getHamiltonianLength(order.begin(), order.end()) << '\n';
+
+        // Algo:
+        // dla kolejnych wierzchołków końcowych:
+        //   jeżeli nic się nie poprawiło w całej sekwencji n ostatnich wierzchołków końcowych - koniec
+        //   stwórz 15 kolejnych ścieżek z najleszych wariantów N-I oraz 2-opt pomijając krawędzie już wykorzystane
+        //   wybierz najlepszy wariant na nową ścieżkę
     }
-    return 0;
 }
